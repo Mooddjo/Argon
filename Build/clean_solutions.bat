84620dfbc88275413e8da062aca4c021770e52bb
Jonathan Depiets <jonathan.depiets@ubisoft.com>
2019-10-18 09:23:11 +0200

    update premake file

---
+++ Build/clean_solutions.bat
@@ -0,0 +1,8 @@
+del Argon.sln
+
+del ..\Sources\Argon\Argon.vcxproj
+del ..\Sources\Argon\Argon.vcxproj.*
+
+del ..\Sources\Playground\Playground.vcxproj
+del ..\Sources\Playground\Playground.vcxproj.*
+pause
