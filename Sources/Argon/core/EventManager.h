da8fc80c70262a40c366527f9f55a7a13d1ccf28
mooddjo <mooddjo@gmail.com>
2019-10-21 23:52:00 +0200

    Start impl of Event Manager

--- Sources/Argon/core/EventManager.h
+++ Sources/Argon/core/EventManager.h
@@ -1,19 +1,29 @@
 #pragma once
+#include <functional>
+#include <list>
 #include "tools/Singleton.h"
+#include "core/StringId.h"
 
 namespace Ar
 {
 	class Event
 	{
-
+		StringId name;
 	};
 
 	class EventManager : public Singleton<EventManager>
 	{
+		using EventFunction = std::function<void(const Event&)>;
+		using EventHandlers = std::list<EventFunction>;
+
 	public:
-		void subscribe();
-		void unsubscribe();
-		void fire(Event& event);
-		void fireWithDelay(int ms);
+		void subscribe(StringId name);
+		void unsubscribe(StringId Name);
+		void fire(const Event& event);
+		void fireWithDelay(const Event& event, int ms);
+
+	protected:
+		std::list<EventFunction>			m_eventHandlers;
+		std::map<StringId, EventHandlers>	m_eventHandlersMap;
 	};
 }
