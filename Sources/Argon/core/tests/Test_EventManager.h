8d80143c385f7cebfcd5d27c27b096521bcf8eb6
DjO <jonathan.depiets@outlook.com>
2020-02-12 20:43:08 +0100

    move Test_EventManager files

---
+++ Sources/Argon/core/tests/Test_EventManager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Test_EventManager
+{
+public:
+	Test_EventManager() = delete;
+	Test_EventManager(const Test_EventManager&) = delete;
+	Test_EventManager& operator=(const Test_EventManager&) = delete;
+
+	static void run();
+};
+
